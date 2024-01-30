8a55d6b90ad3e877cdd44e021a74824625423757
unknown <202280295@psu.palawan.edu.ph>
2024-01-30 20:31:03 +0800

    adding the binary operation

--- midterm.py
+++ midterm.py
@@ -19,7 +19,40 @@ def conversion_menu():
 
 '''------------------------------------------------------------------------------------------------------------------------------------'''
 
+'''Functions for binary operation (Division, Multiplication, Subtraction, Addition, Negative (2's Complement))'''
 
+def division():
+    pass
+
+def multiplication():
+    pass
+
+def subtraction():
+    pass
+
+def addition():
+    pass
+
+def negative_complement():
+    pass
+
+'''Menu for Binary Operation'''
+def binary_operations():
+    binary_operations_menu()
+    operation_choice = int(input("Enter your choice: "))
+    
+    if operation_choice == 1:
+        pass
+    elif operation_choice == 2:
+        pass
+    elif operation_choice == 3:
+        pass
+    elif operation_choice == 4:
+        pass
+    elif operation_choice == 5:
+        pass
+    else:
+        print("Invalid choice")
 
 
 '''------------------------------------------------------------------------------------------------------------------------------------'''
