bb8ce42a85726c49b537511b25cbd4fb7913036b
Christian Ala <202280241@psu.palawan.edu.ph>
2024-02-06 21:48:34 +0800

    adding Binary Operation

--- midterm.py
+++ midterm.py
@@ -17,49 +17,114 @@ def conversion_menu():
     print("[3] Octal to X")
     print("[4] Hexadecimal to X")
 
-'''------------------------------------------------------------------------------------------------------------------------------------'''
-
-'''Functions for binary operation (Division, Multiplication, Subtraction, Addition, Negative (2's Complement))'''
-
-def division():
-    pass
-
-def multiplication():
-    pass
-
-def subtraction():
-    pass
-
-def addition():
-    pass
-
-def negative_complement():
-    pass
-
-'''Menu for Binary Operation'''
+'''Menu for binary operation'''
 def binary_operations():
     binary_operations_menu()
     operation_choice = int(input("Enter your choice: "))
     
     if operation_choice == 1:
-        pass
+        def binary_division(dividend, divisor):
+            dividend_int = int(dividend, 2)
+            divisor_int = int(divisor, 2)
+            quotient_int = 0
+            remainder_int = 0
+            for i in range(len(dividend)):
+                remainder_int = (remainder_int << 1) | int(dividend[i])
+                if remainder_int >= divisor_int:
+                    remainder_int -= divisor_int
+                    quotient_int |= (1 << (len(dividend) - 1 - i))
+            quotient_bin = format(quotient_int, 'b')
+            remainder_bin = format(remainder_int, 'b')
+            return quotient_bin, remainder_bin
+
+        dividend = input("Enter the dividend in binary: ")
+        divisor = input("Enter the divisor in binary: ")
+        quotient, remainder = binary_division(dividend, divisor)
+        print("Quotient:", quotient)
+        print("Remainder:", remainder)
+        
     elif operation_choice == 2:
-        pass
+        def binary_multiplication(a, b):
+            product = 0
+            a_int = int(a, 2)
+            b_int = int(b, 2)
+            while b_int != 0:
+                if b_int & 1:
+                    product += a_int
+                a_int <<= 1
+                b_int >>= 1
+            return format(product, 'b')
+
+        factor1 = input("Enter the first factor in binary: ")
+        factor2 = input("Enter the second factor in binary: ")
+        result = binary_multiplication(factor1, factor2)
+        print("Product:", result)
+        
     elif operation_choice == 3:
-        pass
+        def binary_subtraction(a, b):
+            result_int = int(a, 2) - int(b, 2)
+            if result_int == 0:
+                return '0'
+            elif result_int < 0:
+                result_binary = decimal_to_binary(-result_int)
+                return '-' + result_binary
+            else:
+                return decimal_to_binary(result_int)
+
+        minuend = input("Enter the minuend in binary: ")
+        subtrahend = input("Enter the subtrahend in binary: ")
+        difference = binary_subtraction(minuend, subtrahend)
+        print("Difference:", difference)
+        
     elif operation_choice == 4:
-        pass
+        def add_binary(a, b):
+            max_length = max(len(a), len(b))
+            a = '0' * (max_length - len(a)) + a
+            b = '0' * (max_length - len(b)) + b
+
+            result = ''
+            carry = 0
+
+            for i in range(max_length - 1, -1, -1):
+                digit_sum = int(a[i]) + int(b[i]) + carry
+                result = str(digit_sum % 2) + result
+                carry = digit_sum // 2
+
+            if carry:
+                result = '1' + result
+
+            return result
+
+        binary1 = input("Enter the first binary number: ")
+        binary2 = input("Enter the second binary number: ")
+        print("Sum:", add_binary(binary1, binary2))
+
     elif operation_choice == 5:
-        pass
+        def twos_complement(binary):
+            complement = ''.join('1' if bit == '0' else '0' for bit in binary)
+            
+            result = ''
+            carry = 1
+            for bit in complement[::-1]:
+                if bit == '0' and carry == 1:
+                    result = '1' + result
+                    carry = 0
+                elif bit == '1' and carry == 1:
+                    result = '0' + result
+                else:
+                    result = bit + result
+            
+            return result
+
+        binary = input("Enter a binary number: ")
+        negative = twos_complement(binary)
+        print("Negative (2's complement):", negative)
+
     else:
         print("Invalid choice")
 
 
-'''------------------------------------------------------------------------------------------------------------------------------------'''
-
-'''Functions for converting binary, decimal, hexadecimal, and octal'''
-
-''''BINARY TO DECIAML, HEXADECIMAL, AND OCTAL'''
+''''BINARY TO DECIMAL, HEXADECIMAL, AND OCTAL'''
 def binary_to_decimal(binary):
     decimal = int(binary, 2)
     return decimal
@@ -131,7 +196,6 @@ def number_system_conversion():
         print("Decimal:   ", decimal_output)
         print("Hexa:      ", hexadecimal_output)
         print("Octal:     ", octal_output)
-
         
     elif conversion_choice == 2:
         decimal_input = int(input("Input Decimal: "))
@@ -162,10 +226,7 @@ def number_system_conversion():
 
     else:
         print("Invalid choice")
-
-
-'''------------------------------------------------------------------------------------------------------------------------------------'''
-
+        
 def main_menu():
     while True:
         print("\nMain Menu:")
